b59eb9a8059a8525270e3584207c46a40c51047e
Eren Eroğlu <108634315+ereneroglum@users.noreply.github.com>
2024-07-30 10:01:39 +0300

    Improved documentation.

---
+++ README.md
@@ -0,0 +1,64 @@
+# Sentinel2Video
+
+This is a simple tool to convert images from sentinel 2 satellite to video.
+
+## Installation
+
+Use `pip install .`. Alternatively you can use following commands to install 
+dependencies in a virtual environment:
+
+```bash
+python3 -m venv .venv
+. .venv/bin/activate
+pip install -r requirements.txt
+```
+
+After that use `python3 ./sentinel2video` to execyte the script.
+
+## Usage
+
+### Synopsis
+
+**sentinel2video** [-h | --help]
+
+**sentinel2video** --start START
+                   --end END
+                   [--project PROJECT]
+                   [--fps FPS]
+                   [--bands BANDS [BANDS ...]]
+                   [--gamma GAMMA [GAMMA ...]]
+                   [--min MIN [MIN ...]]
+                   [--max MAX [MAX ...]]
+                   [--output OUTPUT]
+                   [--scale SCALE]
+                   [--tiles TILES [TILES ...]]
+                   [--polling POLLING]
+
+### Options
+
+* **-h**, **--help**  
+  Show help message and exit.
+* **--start START**  
+  Start date of the video.
+* **--end END**  
+  End date of the video.
+* **--project PROJECT**  
+  Project name on google cloud. Defaults to **sentinel2video**.
+* **--fps FPS**  
+  Framerate of the video. Defaults to **1**.
+* **--bands BANDS [BANDS ...]**  
+  Spectral bands. Defaults to **B4 B3 B2**.
+* **--gamma GAMMA [GAMMA ...]**  
+  Gamma(s) for each band. Defaults to **0.95 1.0 1.0**.
+* **--min MIN [MIN ...]**  
+  Value(s) to map to 0 for each band. Defaults to **0.0 0.0 0.0**.
+* **--max MAX [MAX ...]**  
+  Value(s) to map to 255 for each band. Defaults to **3000.0 3000.0 3000.0**.
+* **--output OUTPUT**  
+  Output filename of rendered video. Defaults to **out**.
+* **--scale SCALE**  
+  Scale. Defaults to **100**.
+* **--tiles TILES [TILES ...]**  
+  US-Military Grid Reference System (MGRS) tile(s). Defaults to **31UET**.
+* **--polling POLLING**  
+  Polling interval in seconds. Defaults to **5**.
--- sentinel2video.py
+++ sentinel2video.py
@@ -8,18 +8,18 @@ if __name__ == '__main__':
   parser = argparse.ArgumentParser(prog='sentinel2video',
                                   description='Turn sentinel 2 satellite images to video.')
 
+  parser.add_argument('--start', help='Start date of the video.', type=str, required=True)
+  parser.add_argument('--end', help='End date of the video.', type=str, required=True)
   parser.add_argument('--project', help='Project name on google cloud.', type=str, default='sentinel2video')
-  parser.add_argument('--start', help='Start date of the video', type=str, required=True)
-  parser.add_argument('--end', help='End date of the video', type=str, required=True)
-  parser.add_argument('--fps', help='Framerate of the video', type=int, default=1)
-  parser.add_argument('--bands', help='Bands', nargs='+', default=['B4', 'B3', 'B2'])
-  parser.add_argument('--gamma', help='Gamma(s) for each band', nargs='+', default=[0.95, 1.0, 1.0])
-  parser.add_argument('--min', help='Value(s) to map to 0 for each band', nargs='+', default=[0.0, 0.0, 0.0])
-  parser.add_argument('--max', help='Gamma(s) to map to 255 for each band', nargs='+', default=[3000.0, 3000.0, 3000.0])
-  parser.add_argument('--output', help='Output filename of rendered video', type=str, default='out')
-  parser.add_argument('--scale', help='Scale', type=int, default=100)
+  parser.add_argument('--fps', help='Framerate of the video.', type=int, default=1)
+  parser.add_argument('--bands', help='Spectral bands.', nargs='+', default=['B4', 'B3', 'B2'])
+  parser.add_argument('--gamma', help='Gamma(s) for each band.', nargs='+', default=[0.95, 1.0, 1.0])
+  parser.add_argument('--min', help='Value(s) to map to 0 for each band.', nargs='+', default=[0.0, 0.0, 0.0])
+  parser.add_argument('--max', help='Value(s) to map to 255 for each band.', nargs='+', default=[3000.0, 3000.0, 3000.0])
+  parser.add_argument('--output', help='Output filename of rendered video.', type=str, default='out')
+  parser.add_argument('--scale', help='Scale.', type=int, default=100)
   parser.add_argument('--tiles', help='US-Military Grid Reference System (MGRS) tile(s)', nargs='+', default=['31UET'])
-  parser.add_argument('--polling', help='Polling interval', type=int, default=5)
+  parser.add_argument('--polling', help='Polling interval in seconds.', type=int, default=5)
 
   args = parser.parse_args()
 
